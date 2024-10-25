7b82fe50325e85d829c06ba7431e7711499550b5
Uriel Castillo <uri61220@gmail.com>
2024-10-25 12:05:05 -0600

    controllers id

--- stock_manager/src/server.js
+++ stock_manager/src/server.js
@@ -1,19 +1,35 @@
-const express = require ('express');
+const express = require('express');
+const usersRoutes = require('./routes/users');
+
+
 const app = express();
 
-class Server{
-    constructor () {
+class Server {
+    constructor() {
         this.app = express();
         this.port = 3000;
+
+        this.middlewares();
+
+        this.routes();
+    }
+
+    middlewares(){
         this.app.use(express.json());
+    }
+    routes() {
+        /*this.app.get('/', (req, res) => {
+            res.send('Hello world!');
+        });*/
 
+        this.app.use('/users', usersRoutes);
+    }
+
+    start() {
+        this.app.listen(this.port, () => {
+            console.log('Example app listening on port 3000! ' + this.port);
+        });
+    }
 }
 
-start(){
-    this.app.listen(this.port, () =>{
-        console.log('Example app listening on port' + this.port);
-    });
-}
-}
-
-module.exports = {Server};
+module.exports = { Server };
