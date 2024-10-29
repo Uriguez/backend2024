45c3901784967a4cbdb48aeeaa145e7deee4f5e0
Uriel Castillo <uri61220@gmail.com>
2024-10-29 11:47:41 -0600

    Crear y actualizar datos

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -28,5 +28,64 @@ const getAll = (req = request, res = response) => {
             return;
         }
         res.send(user);
+
     }
-    module.exports = {getAll, getById};
+
+//CREAR, ACTUALIZAR Y ELIMINAR DATOS
+
+    const createUser = (req = request, res = response) => {
+        const { name } = req.body;
+        if (!name) {
+          res.status(400).send('Name is required');
+          return;
+        }
+        const newUser = { id: users.length + 1, name };
+        users.push(newUser);
+        res.status(201).send(newUser);
+      };
+      
+      // Actualizar un usuario existente
+      const updateUser = (req = request, res = response) => {
+        const { id } = req.params;
+        const { name } = req.body;
+      
+        if (isNaN(id)) {
+          res.status(400).send('Invalid ID');
+          return;
+        }
+      
+        const user = users.find((user) => user.id === +id);
+        if (!user) {
+          res.status(404).send('User not found');
+          return;
+        }
+      
+        if (!name) {
+          res.status(400).send('Name is required');
+          return;
+        }
+      
+        user.name = name;
+        res.send(user);
+      };
+      
+      // Eliminar un usuario
+      const deleteUser = (req = request, res = response) => {
+        const { id } = req.params;
+      
+        if (isNaN(id)) {
+          res.status(400).send('Invalid ID');
+          return;
+        }
+      
+        const index = users.findIndex((user) => user.id === +id);
+        if (index === -1) {
+          res.status(404).send('User not found');
+          return;
+        }
+      
+        users.splice(index, 1);
+        res.status(204).send(); // 204 No Content indica éxito sin contenido adicional
+      };
+      
+      module.exports = { getAll, getById, createUser, updateUser, deleteUser };
